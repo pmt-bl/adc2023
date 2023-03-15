0e2888f930eeaa2998fcfab640fe782b0906241a
pmt-bl <pmtbl.20@gmail.com>
2023-03-15 13:45:15 +0000

    Repo forked

--- src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/ComputationResource.java
+++ src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/ComputationResource.java
@@ -1,8 +1,11 @@
 package pt.unl.fct.di.apdc.firstwebapp.resources;
 
+import java.io.IOException;
+import java.net.URI;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.ws.rs.GET;
@@ -28,9 +31,13 @@ public class ComputationResource {
 	@GET
 	@Path("/hello")
 	@Produces(MediaType.TEXT_PLAIN)
-	public Response hello() {
-		LOG.fine("Saying hello!!");
-		return Response.ok().entity("Hello human! I hope you are having a fine day.").build();
+	public Response hello() throws IOException{
+		try {
+			throw new IOException("UPS");
+		} catch (Exception e) {
+			LOG.log(Level.SEVERE, "Exception on Method /hello", e);
+			return Response.temporaryRedirect(URI.create("/error/500.html")).build();
+		}
 	}
 	
 	@GET
